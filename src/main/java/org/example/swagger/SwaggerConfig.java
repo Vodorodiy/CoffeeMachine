df4d6cbe98ae24b76e50090d01d0a1b6b41c29fd
Vodorodiy <Vodorodiy00@yandex.ru>
2024-09-13 11:09:36 +0300

    refactor

--- src/main/java/org/example/swagger/SwaggerConfig.java
+++ src/main/java/org/example/swagger/SwaggerConfig.java
@@ -7,21 +7,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-    @OpenAPIDefinition()
-    public class SwaggerConfig {
-    public SwaggerConfig() {
-
-    }
+@OpenAPIDefinition()
+public class SwaggerConfig {
 
     @Bean
     public OpenAPI swaggerApiConfig() {
-    var info = new Info()
-    .title("CoffeMachine API")
-    .description("SwaggerUI for testing coffee machine")
-    .version("1.0");
-    return new OpenAPI().info(info);
+        var info = new Info()
+                .title("CoffeMachine API")
+                .description("SwaggerUI for testing coffee machine")
+                .version("1.0");
+        return new OpenAPI().info(info);
     }
 
-    }
+}
 
 
